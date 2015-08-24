9b4acdd5cccd21625395c021709e695d395ecbba
Tianqi Zhang <1120404010@qq.com>
2015-08-24 15:24:51 +0800

    Initial Commit

---
+++ OC_Lesson_8_24_KVC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC_Lesson_8_24_KVC
+//
+//  Created by 张天琦 on 15/8/24.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
